04ac5a43c9a167c09a7661bc87e2fc7255667a1c
Toma Leto <echolon@ya.ru>
2022-07-30 19:16:34 +0300

    Add autotests for unreg users

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        int expected = 30;
+
+        long actual = service.calculate(1_000, true);
+        System.out.println(expected + " == ? == " + actual);
+
+        int expectedNoReg = 10;
+
+        long actualNoReg = service.calculate(1_000, false);
+        System.out.println(expectedNoReg + " == ? == " + actualNoReg);
+    }
 }
+
--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -1,38 +1,68 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BonusServiceTest {
-    import static org.junit.jupiter.api.Assertions.org.junit.jupiter.api.Test;
 
-    public class BonusServiceTest {
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
 
-        @org.junit.jupiter.api.Test
-        void shouldCalculateForRegisteredAndUnderLimit() {
-            BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = true;
+        long expected = 30;
 
-            // подготавливаем данные:
-            long amount = 1000;
-            boolean registered = true;
-            long expected = 30;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
 
-            // вызываем целевой метод:
-            long actual = service.calculate(amount, registered);
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 
-            // производим проверку (сравниваем ожидаемый и фактический):
-            assertEquals(expected, actual);
-        }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
 
-        @org.junit.jupiter.api.Test
-        void shouldCalculateForRegisteredAndOverLimit() {
-            BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = true;
+        long expected = 500;
 
-            // подготавливаем данные:
-            long amount = 1_000_000;
-            boolean registered = true;
-            long expected = 500;
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
 
-            // вызываем целевой метод:
-            long actual = service.calculate(amount, registered);
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 
-            // производим проверку (сравниваем ожидаемый и фактический):
-            assertEquals(expected, actual);
-        }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1000;
+        boolean registered = false;
+        long expectedNoReg = 10;
+
+        // вызываем целевой метод:
+        long actualNoReg = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expectedNoReg, actualNoReg);
+    }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expectedNoReg = 500;
+
+        // вызываем целевой метод:
+        long actualNoReg = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expectedNoReg, actualNoReg);
     }
 }
